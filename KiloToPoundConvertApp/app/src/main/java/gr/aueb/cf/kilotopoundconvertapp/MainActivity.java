46ad6dbf75a6f436ac66e2ff3e8d347c308a2d1d
serendipity019 <navagio09@gmail.com>
2026-01-27 17:55:30 +0200

    The app is ready! The mainActivity have try and catch in case someone don't give proper input

--- KiloToPoundConvertApp/app/src/main/java/gr/aueb/cf/kilotopoundconvertapp/MainActivity.java
+++ KiloToPoundConvertApp/app/src/main/java/gr/aueb/cf/kilotopoundconvertapp/MainActivity.java
@@ -1,6 +1,9 @@
 package gr.aueb.cf.kilotopoundconvertapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +11,48 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+
 public class MainActivity extends AppCompatActivity {
+    private EditText kiloET;
+
+    private TextView convPoundTV;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        MaterialButton convertBtn = findViewById(R.id.convertBtn);
+        kiloET = findViewById(R.id.kiloET);
+        convPoundTV = findViewById(R.id.convPoundTV);
+
+        convertBtn.setOnClickListener(new View.OnClickListener()  {
+
+            @Override
+            public void onClick(View v) {
+                String input = kiloET.getText().toString();
+
+                if (!input.isEmpty()){
+                    try {
+                        float kilo = Float.parseFloat(input); //convert the string to float
+                        float pound = kilo * 2.20462f;
+
+                        String poundString = String.format("%.2f", pound);
+                        convPoundTV.setText(poundString);
+
+                        kiloET.setError(null);
+                    } catch (NumberFormatException e) {
+                        kiloET.setError("Invalid Input, give a number");
+                    }
+                } else {
+                    kiloET.setError("Field cannot be empty");
+                }
+            }
         });
+
     }
 }
